d08c3a87c35a1cb16ffff1e95cc38924609f4f60
Mayuresh Kamble <mackam30@gmail.com>
2025-09-22 23:58:11 +0530

    added socket but not functional yet

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,23 @@
-c
+import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'  
+
+const app = express()
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+})
+
+io.on("connection", (socket) => {
+    console.log("User is connected", socket.id)
+})
+
+
+
+const port = process.env.PORT || 5000
+server.listen(port, () =>{
+    console.log("Server is running on Port 5000")
+})
