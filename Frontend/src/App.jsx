bc883b1cad13fbb90c15ec38fd76b499a45b7321
Mayuresh Kamble <mackam30@gmail.com>
2025-09-24 15:25:42 +0530

    adding more joining functionality

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,24 @@
 import { useState } from "react"
 import "./App.css"
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 
 const socket = io("http://localhost:5000")
 
 const App = () => {
-  
-  const [joined, setJoined] = useState(false)
 
-  if(!joined){
-    return <div>App not joined</div>
+  const [joined, setJoined] = useState(false)
+  const [roomId, setRoomId] = useState("")
+  const [userName, setuserName] = useState("")
+
+  if (!joined) {
+    return <div className="">
+      <div className="join-form">
+        <h1>Join Code Room</h1>
+        <input type="text"
+          placeholder="Room Id"
+          value={roomId} onChange={(e) => setRoomId(e.target.value) } />
+      </div>
+    </div>
   }
   return <div>User joined </div>
 }
